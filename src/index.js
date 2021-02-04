1814378d41b63451716164dc93934b5b1faa0b38
sberdine77 <sbsx77xavier@hotmail.com>
2021-02-04 00:24:15 -0300

    Adding dotenv

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
+app.use(bodyParser.json)
 
 app.get('/', (req, res) => {
 	return res.send("Hello World!");
 })
 
-app.listen(3000);
+app.listen(process.env.PORT || 5000);
